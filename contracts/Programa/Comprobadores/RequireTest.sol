dade7796a5341da59f9c4c3d8ed3498e98dbbde9
sulvank <samuelcasanova.int@gmail.com>
2025-03-22 10:39:16 +0000

    feat: different types of error control

--- contracts/Programa/Comprobadores/RequireTest.sol
+++ contracts/Programa/Comprobadores/RequireTest.sol
@@ -4,20 +4,28 @@
 // Version 
 pragma solidity 0.8.28;
 
+error RequireNotOwner(address);
+
 // Contrato
 contract RequireTest {
 
-    address admin;
+    address public admin;
     constructor(address admin_) {
         admin = admin_;
     }
 
-    // Funciton + if check: msg.sender sea igual a admin
+    // Function + if check: msg.sender sea igual a admin
     function checkAdmin() public view {
         if(msg.sender != admin) revert(); 
     }
 
+    // Function + require check
     function checkAdminRequire() public view {
         require(msg.sender == admin , "msg.sender is not admin");
+    }
+
+    // Function + if + custom error
+    function checkAdminCustomErrors() public view {
+        if(msg.sender != admin) revert RequireNotOwner(msg.sender);
     }
 }
